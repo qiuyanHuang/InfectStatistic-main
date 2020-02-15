354c12db31b7d9892b504af193828b9dfb0f1582
sunny <2453709145@qq.com>
2020-02-15 19:49:05 +0800

    设计疫情统计程序的基本结构

---
+++ 221701309/src/InfectStatistic.java
@@ -0,0 +1,36 @@
+/**
+ * InfectStatistic
+ * TODO
+ *
+ * @author qiuyanhuang
+ * @version 1.0
+ * @since 2020-2-15
+ */
+
+
+
+class InfectStatistic
+{
+
+
+    //命令行类
+    class Command {
+
+        }
+
+
+    //文件类
+    class File{
+
+    }
+
+
+    //主函数
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
+
